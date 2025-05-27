4cc2f4a3b342b8f4e61c961fc8204a42db45ebdc
splnt <splsnitv@gmail.com>
2025-05-27 21:29:22 +0545

    change navbar again

--- test/src/navbar.jsx
+++ test/src/navbar.jsx
@@ -2,12 +2,7 @@ function navbar(){
     return <>
     <div className="row">
 <h3>hello</h3>
-<div className="col-2 bg-primary  p-2"></div>
-<div className="col-2 bg-sucess p-2"></div>
-<div className="col-2 bg-dark p-2"></div>
-<div className="col-2 bg-primary p-2"></div>
-<div className="col-2 bg-sucess p-2"></div>
-<div className="col-2 bg-dark p-2"></div>
+
 </div>
 </>
 
